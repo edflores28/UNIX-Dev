e440016866f561aa56ac0fb1c1540c2c672bad7f
Edwin Flores <edflores28@gmail.com>
2017-02-15 18:28:56 -0500

    Added header information and updated code formatting

--- Project_1/src/bin/conv_to_cent/conv_to_cent.cpp
+++ Project_1/src/bin/conv_to_cent/conv_to_cent.cpp
@@ -1,3 +1,15 @@
+/*
+ *Project: homework 1
+ *
+ *Progam: conv_to_cent - converts a value to centigrade
+ *File Name: cent_convert.cpp
+ *Purpose: obtains a value in fahrenheiht from the cmd line and converts it
+ *         to centigrade
+ *
+ *Programmer: Edwin Flores
+ *Course: EN.605.414.81
+ *
+ */
 #include <iostream>
 #include <iomanip>
 #include "getarg.h"
@@ -5,7 +17,8 @@
 
 using namespace std;
 
-int main(int argc, char* argv[]) {
+int main (int argc, char* argv[])
+{
 
 	int argSuccess = 0;
 	double value = 0.0;
@@ -13,9 +26,11 @@ int main(int argc, char* argv[]) {
 	argSuccess = get_argument(argc, argv, &value);
 
 	if (argSuccess == 0)
-		cout << setprecision(4) << value  << " degrees Fahrenheight is " << setprecision(4) << convert_to_cent(value) << " degree centigrade" << endl;
+	{
+		cout << setprecision(4) << value  << " degrees Fahrenheiht is "; 
+		cout <<	setprecision(4) << convert_to_cent(value);
+		cout << " degree centigrade" << endl;
+	}
 
 	return 0;
-
 }
-
