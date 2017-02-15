53517e59af1bc3a6969c9e9521c99806ae97f869
Edwin Flores <edflores28@gmail.com>
2017-02-15 18:34:02 -0500

    Added header information and updated code formatting

--- Project_1/src/bin/conv_to_fahr/conv_to_fahr.cpp
+++ Project_1/src/bin/conv_to_fahr/conv_to_fahr.cpp
@@ -1,11 +1,23 @@
+/*
+ *Project: homework 1
+ *
+ *Progam: conv_to_fahr - converts a value to fahrenheit
+ *File Name: conv_to_fahr.cpp
+ *Purpose: obtains a value in centigrade from the user and converts it
+ *         to fahrenheit
+ *
+ *Programmer: Edwin Flores
+ *Course: EN.605.414.81
+ *
+ */
 #include <iostream>
 #include <iomanip>
 #include "fahr_convert.h"
 
 using namespace std;
 
-int main() {
-
+int main()
+{
 	double centigrade = 0.0;
 	double value = 0.0;
 
@@ -15,9 +27,8 @@ int main() {
 	
 	value = convert_to_fahr(centigrade);
 
-	cout << setprecision(4) << centigrade << " degree centigrade is " << setprecision(4) << value << " Fahrenheit." << endl; 
+	cout << setprecision(4) << centigrade << " degree centigrade is ";
+	cout << setprecision(4) << value << " Fahrenheit." << endl; 
 
 	return 0;
-
 }
-
