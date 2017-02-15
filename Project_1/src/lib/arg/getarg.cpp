8fe2629babb81b47ae8230ea830bd72c9560e6fe
Edwin Flores <edflores28@gmail.com>
2017-02-15 18:17:55 -0500

    Added header infomation and a comment block

--- Project_1/src/lib/arg/getarg.cpp
+++ Project_1/src/lib/arg/getarg.cpp
@@ -1,8 +1,26 @@
+/*
+ *Project: homework 1
+ *
+ *Library: arg - parse command line library
+ *File Name: getarg.cpp
+ *Purpose: provides a function to retrieve an argument
+ *
+ *Programmer: Edwin Flores
+ *Course: EN.605.414.81
+ *
+ */
 #include <stdlib.h>
 #include "getarg.h"
 
-int get_argument (int argc, char *argv[], double *return_value) {
-	if (argc > 1){
+int get_argument (int argc, char *argv[], double *return_value)
+{
+	// We only care about the second index of argv since
+	// it will contain the value that is needed. The second
+	// value will be converted into a float and indicate
+	// a success.
+	
+	if (argc > 1)
+	{
 		*return_value = atof(argv[1]);
 		return 0;
 	}
