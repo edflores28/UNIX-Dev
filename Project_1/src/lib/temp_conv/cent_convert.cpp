b95a55d9e464bac0179a766468dee7cef5f95556
Edwin Flores <edflores28@gmail.com>
2017-02-15 18:02:05 -0500

    Added header information to the library source files

--- Project_1/src/lib/temp_conv/cent_convert.cpp
+++ Project_1/src/lib/temp_conv/cent_convert.cpp
@@ -1,7 +1,19 @@
+/*
+ *Project: homework 1
+ *
+ *Library: temp_conv - temperature conversion library
+ *File Name: cent_convert.cpp
+ *Purpose: provides a function to convert farenheit to centigrade
+ *
+ *Programmer: Edwin Flores
+ *Course: EN.605.414.81
+ *
+ */
 #include "cent_convert.h"
 
 const double factor = 5.0/9.0;
 
-double convert_to_cent (double fahr) {
+double convert_to_cent (double fahr)
+{
 	return ((fahr - 32.0) * factor);
 }
--- Project_1/src/lib/temp_conv/fahr_convert.cpp
+++ Project_1/src/lib/temp_conv/fahr_convert.cpp
@@ -1,7 +1,19 @@
+/*
+ *Project: homework 1
+ *
+ *Library: temp_conv - temperature conversion library
+ *File Name: fahr_convert.cpp
+ *Purpose: provides a function to convert centigrade to fahrenheit
+ *
+ *Programmer: Edwin Flores
+ *Course: EN.605.414.81
+ *
+ */
 #include "fahr_convert.h"
 
 const double factor = 9.0/5.0;
 
-double convert_to_fahr (double cent) {
+double convert_to_fahr (double cent)
+{
 	return ((cent * factor) + 32.0);
 }
