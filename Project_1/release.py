5ee68fb4c43de4fd45609b4cf1a50193137af5b2
Edwin Flores <edflores28@gmail.com>
2017-02-15 19:52:25 -0500

    Added functionality to create a tar file and obtain the computers hostname

--- Project_1/release.py
+++ Project_1/release.py
@@ -1,24 +1,33 @@
 import argparse
 import sys
+import socket
 import subprocess
 
 parser = argparse.ArgumentParser()
 
-parser.add_argument('-s', action="store_true")
-parser.add_argument('-b', action="store_true")
+parser.add_argument('-s', action="store_true", help ='create a source release')
+parser.add_argument('-b', action="store_true", help='create a binary release')
 
 args = parser.parse_args()
 
 no_arg = args.s or args.b
 
 if no_arg == False:
-    print ("NO VALUE")
+    print ("There was no valid argument")
     sys.exit(0)
+
+host = socket.gethostname()
+
+if host == "absaroka":
+    make = 'gmake'
+else:
+    make = 'make';
 
 if args.s == True:
     user_in = raw_input('Create a source release (Y/N):') 
     if user_in == "Y":
-        subprocess.call(['make', 'clean'])
+        subprocess.call([make,'clean'])
+        subprocess.call(['tar','cvf','homework1.tar','bin','include','lib','src'])
 if args.b == True:
     user_in = raw_input('Create a binary release (Y/N):') 
     print (user_in)
