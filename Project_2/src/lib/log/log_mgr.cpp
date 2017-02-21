21d7671874c847e9e0b7f5188054dd3deb63fdab
Edwin Flores <edflores28@gmail.com>
2017-02-21 17:38:14 -0500

    Added a compiling log manager with a simple makefile

---
+++ Project_2/src/lib/log/log_mgr.cpp
@@ -0,0 +1,17 @@
+#include "log_mgr.h"
+
+
+int log_event(Levels I, const char *fmt, ...)
+{
+	return 1;
+}
+
+int set_logfile(const char *logfile_name)
+{
+	return 1;
+}
+
+void close_logfile(void)
+{
+}
+
