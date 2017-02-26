4f7fcecac584ab9095890605ae294f8594b650a0
Edwin Flores <edflores28@gmail.com>
2017-02-26 15:07:42 -0500

    Added fuctionatily to close and set files. Added functionality to open and write to files

--- Project_2/src/lib/log/log_mgr.cpp
+++ Project_2/src/lib/log/log_mgr.cpp
@@ -1,17 +1,43 @@
+#include <fcntl.h>
+#include <algorithm>
 #include <iostream>
+#include <unistd.h>
 #include <stdarg.h>
 #include <stdio.h>
 #include <string.h>
 #include <time.h>
 #include "log_mgr.h"
+#include <sys/stat.h>
+
+#define BUFFER_SIZE 256
 
 using namespace std;
 
+
 static const char *enumText[] = {"INFO", "WARNING", "FATAL"};
+
+string logName = "logfile";
+int fd = -1;
 
 const char* getText(Levels value)
 {
 	return enumText[value];
+}
+
+int openFile (string log)
+{
+	char buffer[BUFFER_SIZE];
+	int temp = -1;
+
+	memset(&buffer[0], 0, sizeof(buffer));
+	strcpy(buffer, logName.c_str());
+
+	fd = open(buffer, O_APPEND|O_CREAT|O_WRONLY, 0644);
+
+//	temp = fchmod(fd, 666);
+//	cout << temp << "chmod" << endl;
+
+	return fd;
 }
 
 int log_event(Levels I, const char *fmt, ...)
@@ -23,6 +49,7 @@ int log_event(Levels I, const char *fmt, ...)
 	va_list args;
 	string fullString;
 	string formatted;
+	ssize_t bytesWritten = -1;
 
 	// Obtain the current time.
 	time (&rawtime);
@@ -37,7 +64,7 @@ int log_event(Levels I, const char *fmt, ...)
 	fullString.append(getText(I));
 	
 	// Clear the buffer.
-	buffer[0] = 0;
+	memset(&buffer[0], 0, sizeof(buffer));
 
 	// Obtain the arguments to the function.
 	va_start (args, fmt);
@@ -46,18 +73,38 @@ int log_event(Levels I, const char *fmt, ...)
 	// Append the arguments to the string
 	fullString.append(1u,':');
 	fullString.append(string(buffer));
+	fullString.append(1u, '\n');
 
 	cout << fullString << endl;
-		
+	
+	memset(&buffer[0], 0, sizeof(buffer));
+	strcpy(buffer, logName.c_str());
+	
+	if (fd == -1)
+		openFile(logName);
+
+	memset(&buffer[0], 0, sizeof(buffer));
+	strcpy(buffer, fullString.c_str());
+
+	bytesWritten = write(fd, buffer, fullString.length());
+
+	cout << "FD: " << fd << " Bytes: " << bytesWritten << endl;
+
 	return 1;
 }
 
 int set_logfile(const char *logfile_name)
 {
-	return 1;
+	logName = string(logfile_name);
+	
+	if (openFile(logName) == -1)
+		return -1;
+
+	return 0;
 }
 
 void close_logfile(void)
 {
+	close(fd);
 }
 
