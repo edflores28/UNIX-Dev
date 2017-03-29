63a34e162bfa777069766ecaed8d0175cc836fbf
Edwin Flores <edflores28@gmail.com>
2017-03-28 20:10:15 -0400

    Updated argument processing

--- Project_3/src/bin/app1/app1.cpp
+++ Project_3/src/bin/app1/app1.cpp
@@ -1,20 +1,22 @@
 /*
  *Project: homework 3
  *
- *Progam: app1 - TBD
+ *Progam: app1 - Allows the user to create a certain amount of
+ *               threads. The program will wait until all threads
+ *               are completed.
+ *
  *File Name: app1.cpp
- *Purpose: TBD
  *
  *Programmer: Edwin Flores
  *Course: EN.605.414.81
  *
  */
 #include <iostream>
-#include "getarg.h"
 #include "log_mgr.h"
 #include "thread_mgr.h"
-#include <unistd.h>
 #include <pthread.h>
+#include <stdlib.h>
+#include <unistd.h>
 
 void *computation (void *)
 {
@@ -28,30 +30,31 @@ void *computation (void *)
 
 int main(int argc, char *argv[])
 {
-	
-	int argSuccess = -1;
-	double value = 0.0;
-	int totalThreads = -1;
-
-	argSuccess = get_argument(argc, argv, &value);
-	totalThreads = (int) value;
-
-	if ((argSuccess == OK) && (totalThreads >= 1) && (totalThreads <= 12))
+	if (argc !== 2)
 	{
-		
-		ThreadHandles handle = -1;
-		int waitAll = -1;
-
-		for (int i = 0; i < 12; i++)
-			handle = th_execute(computation);
-
-		waitAll = th_wait_all();
-
-		std::cout << "wait: " << waitAll <<  std::endl;
-
-		return 0;
+		std::cout << "Invalid arguments" << std::endl;
+		return -1;
 	}
-	std::cout << "ERROR" << std::endl;
-	return -1;
-}
 
+	int totalThreads = atoi(argv[1]);
+	
+	bool validThreads = (totalThreads >= 1) && (totalThreads <= 12);
+
+	if (!validThreads)
+	{
+		std::cout << "Total threads not in valid range (1-12)" << std::endl;
+		return -1;
+	}
+
+	ThreadHandles handle = -1;
+	int waitAll = -1;
+
+	for (int i = 0; i < 12; i++)
+		handle = th_execute(computation);
+
+	waitAll = th_wait_all();
+
+	std::cout << "wait: " << waitAll <<  std::endl;
+
+	return 0;
+}
