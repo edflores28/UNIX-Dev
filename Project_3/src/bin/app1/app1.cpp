fe0697d51c1045e1c105b96ddcea7961d1c87db1
Edwin Flores <edflores28@gmail.com>
2017-03-30 19:23:33 -0400

    Added current implementation of app1

--- Project_3/src/bin/app1/app1.cpp
+++ Project_3/src/bin/app1/app1.cpp
@@ -20,17 +20,14 @@
 
 void *computation (void *)
 {
-	std::cout << " start" << std::endl;
-	
-	for (int i = 0; i <= 1000000000; i++);
+	for (int i = 0; i <= 2111000000; i++);
 
-	std::cout << " end" << std::endl;
 	int kill = th_exit();
 }
 
 int main(int argc, char *argv[])
 {
-	if (argc !== 2)
+	if (argc != 2)
 	{
 		std::cout << "Invalid arguments" << std::endl;
 		return -1;
@@ -49,12 +46,10 @@ int main(int argc, char *argv[])
 	ThreadHandles handle = -1;
 	int waitAll = -1;
 
-	for (int i = 0; i < 12; i++)
+	for (int i = 0; i < totalThreads; i++)
 		handle = th_execute(computation);
 
 	waitAll = th_wait_all();
-
-	std::cout << "wait: " << waitAll <<  std::endl;
 
 	return 0;
 }
