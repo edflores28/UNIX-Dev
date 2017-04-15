614fafa09a6d292929bd7a9db4be364b65ebc881
Edwin Flores <edflores28@gmail.com>
2017-04-15 09:54:18 -0400

    Added skeleton and makefile for install_data

---
+++ Project_4/src/bin/install_data/install_data.cpp
@@ -0,0 +1,19 @@
+/*
+ *Project: homework 3
+ *
+ *Progam: app1 - 
+ *
+ *File Name: install_data.cpp
+ *
+ *Programmer: Edwin Flores
+ *Course: EN.605.414.81
+ *
+ */
+#include <iostream>
+#include "shared_mem.h"
+
+int main()
+{
+
+	return 0;
+}
