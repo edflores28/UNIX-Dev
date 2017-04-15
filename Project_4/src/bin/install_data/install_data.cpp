0fa76bac06b95c176e9243d2ac49fe81b7d34725
Edwin Flores <edflores28@gmail.com>
2017-04-15 13:37:01 -0400

    Added additional functionality

--- Project_4/src/bin/install_data/install_data.cpp
+++ Project_4/src/bin/install_data/install_data.cpp
@@ -14,6 +14,7 @@
 #include <sstream>
 #include <string.h>
 #include <stdlib.h>
+#include <unistd.h>
 #include "common_struct.h"
 #include "shared_mem.h"
 
@@ -48,15 +49,26 @@ int main (int argc, char *argv[])
 	if (!readFile)
 	{
 		cout << "ERROR OPENING FILE";
-		return 0;
+		return -1;
 	}
 
+	int arraySize = sizeof(shared) * 20;
+	
+	shared *shmArry;
+
+	void *ptr = connect_shm(1000, arraySize);
+
+	shmArry = (shared *) ptr;
 
 	while (getline(readFile, line))
 	{
+		// Obtain the current line and obtain the string
+		// representations of the values.
 		istringstream iss (line);
 		iss >> skipws >> index_str >> x_str >> y_str >> time_str;
 
+		// The follwing lines of code converts the string
+		// representation of their values into their types.
 		strcpy(buffer,x_str.c_str());
 		x = atof(buffer);
 		
@@ -72,7 +84,25 @@ int main (int argc, char *argv[])
 		strcpy(buffer,time_str.c_str());
 		time = atoi(buffer);
 
-		cout << index << " " << x << " " << y << " " << time << endl;
+		if (time >= 0)
+		{
+			sleep(time);
+			shmArry[index].x = x;
+			shmArry[index].y = y;
+			shmArry[index].is_valid = 1;
+		}
+		else
+		{
+			// Otherwise the time is negative.
+			time = abs(time);
+			sleep (time);
+			shmArry[index].is_valid = 0;
+		}
+	}
+
+	for (int i = 0; i < 20; i++) 
+	{
+//		cout << "x: " << shmArry[i].x << "y: " << shmArry[i].y << endl;
 	}
 
 	return 0;
