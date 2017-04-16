1808438dcb144c527f4f290d9f2459da426617c4
Edwin Flores <edflores28@gmail.com>
2017-04-16 08:40:11 -0400

    Added header

--- Project_4/include/common_struct.h
+++ Project_4/include/common_struct.h
@@ -1,3 +1,15 @@
+/*
+ *Project: homework 4
+ *
+ *File Name: common_struct.h
+ *Purpose: Defines the common shared structure
+ *
+ *Programmer: Edwin Flores
+ *Course: EN.605.414.81
+ *
+ */
+#define ARRAY_LENGTH 20
+
 struct shared
 {
 	int is_valid;
