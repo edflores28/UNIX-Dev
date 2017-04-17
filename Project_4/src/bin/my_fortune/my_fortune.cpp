2f856ba1b8aae95f851124801a48fdcbd9850682
Edwin Flores <edflores28@gmail.com>
2017-04-17 19:29:09 -0400

    Added skeleton and makfile

---
+++ Project_4/src/bin/my_fortune/my_fortune.cpp
@@ -0,0 +1,18 @@
+/*
+ *Project: homework 4
+ *
+ *Progam: my_fortune - This program reads from a pipe and converts
+ *                     fortunes to upper case.
+ *
+ *File Name: my_fortune.cpp
+ *
+ *Programmer: Edwin Flores
+ *Course: EN.605.414.81
+ *
+ */
+#include <iostream>
+
+int main(int argc, char *argv[])
+{
+	return 0;
+}
