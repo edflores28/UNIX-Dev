5c18ee1c79ddbca3c3a7b8d29f9a1c76857988ec
Edwin Flores <edflores28@gmail.com>
2017-04-20 17:26:39 -0400

    Tweaked and cleaned up the file

--- Project_4/src/bin/monitor_shm/monitor_shm.cpp
+++ Project_4/src/bin/monitor_shm/monitor_shm.cpp
@@ -26,30 +26,31 @@ int Counter = 0;
 
 int main (int argc, char *argv[])
 {
+	// Variables
 	int monitor = 30;
+	int arraySize = sizeof(shared) * 20;
+	shared *shmArry;
+	float x = 0.0;
+	float y = 0.0;
+	int totalValid = 0;
 
+
+	// Check to see if there was an argument
 	if (argc == 2)
 	{
 		monitor = atoi(argv[1]);
 	}
 
-	cout << "monitor: " << monitor << endl;
-
-	int arraySize = sizeof(shared) * 20;
-	
-	shared *shmArry;
-
-	void *ptr = connect_shm(1000, arraySize);
-
+	// Obtain the shared region pointer
+	void *ptr = connect_shm(SHM_KEY, arraySize);
 	shmArry = (shared *) ptr;
 
-
-	float x = 0.0;
-	float y = 0.0;
-	int totalValid = 0;
-
+	// While loop that contains bulk of the processing
+	// and breaks when the counter hits the monitor count.
 	while (Counter != monitor)
 	{
+		// Find all valid indexes and update the x, y
+		// and total valid values.
 		for (int i = 0; i < ARRAY_LENGTH; i++) 
 		{
 			if (shmArry[i].is_valid == 1)
@@ -60,6 +61,7 @@ int main (int argc, char *argv[])
 			}
 		}
 		
+		// Print out the necessary information.
 		if (totalValid == 0)
 			cout << "At time " << Counter << ": no elements are active" << endl;
 		else
